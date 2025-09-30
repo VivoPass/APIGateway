f1990c750f21dba3d64a8759a79e4e25b7ae0512
mariibarra14 <mariangelicaibarraluces@gmail.com>
2025-09-30 17:26:52 -0400

    initial commit
    Estructura básica de la API Gateway

---
+++ Gateway.API/Controllers/GatewayController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayController : ControllerBase
+    {
+    
+    }
+}
