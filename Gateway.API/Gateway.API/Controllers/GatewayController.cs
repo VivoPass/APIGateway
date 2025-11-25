ee19fbc3a2ce945beadb08288a540013994eb19e
mariibarra14 <mariangelicaibarraluces@gmail.com>
2025-11-25 01:36:28 -0400

    FIX auditorias + conexión con MS Usuarios
    Agrego la funcionalidad de las auditorias + conexión con el MS de usuario para realizar la publicación en el historial de actividad

--- Gateway.API/Gateway.API/Controllers/GatewayController.cs
+++ Gateway.API/Gateway.API/Controllers/GatewayController.cs
@@ -1,14 +1,20 @@
-﻿using log4net;
+﻿using Gateway.API.DTOs;
 using Gateway.Infrastructure.DTOs;
-using Gateway.API.DTOs;
 using Gateway.Infrastructure.Interfaces;
+using log4net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using RestSharp;
+using System.Net;
 using System.Security.Claims;
 
 namespace Gateway.API.Controllers
 {
+    /// <summary>
+    /// Controlador principal que actúa como API Gateway, gestionando la autenticación (Login, Registro, Contraseña)
+    /// y enrutando peticiones a los microservicios internos, principalmente Keycloak.
+    /// </summary>
     [ApiController]
     [Route("/")]
     public class GatewayController : ControllerBase
@@ -17,17 +23,28 @@ namespace Gateway.API.Controllers
         private readonly ISendEmailService _sendEmailService;
         private readonly IKeycloakService _keycloakService;
         private readonly ILog Logger;
+        private readonly IAuditoriaRepository AuditoriaRepository;
 
-        public GatewayController(IRestClient restClient, ISendEmailService sendEmailService, IKeycloakService keycloakService, ILog logger)
+        public GatewayController(IRestClient restClient, ISendEmailService sendEmailService, IKeycloakService keycloakService, ILog logger, IAuditoriaRepository auditoriaRepository)
         {
             _restClient = restClient;
             _sendEmailService = sendEmailService;
             _keycloakService = keycloakService;
             Logger = logger;
+            AuditoriaRepository = auditoriaRepository;
         }
 
         #region Login
+        /// <summary>
+        /// Permite a un usuario autenticarse en el sistema.
+        /// Procesa la autenticación en Keycloak y publica una actividad en el MS de Usuarios.
+        /// </summary>
+        /// <param name="request">Datos de inicio de sesión (correo y contraseña).</param>
+        /// <returns>Objeto con tokens de acceso y datos de usuario si es exitoso (200 OK).</returns>
         [HttpPost("login")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(object))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> Login([FromBody] loginDto request)
         {
             Logger.Info($"Iniciando intento de login para el correo: {request.Correo}");
@@ -51,6 +68,31 @@ namespace Gateway.API.Controllers
 
                 Logger.Info($"Usuario {request.Correo} autenticado con éxito. ID: {idUsuario}, Rol: {idRol}.");
 
+                //Conexion con el MS de Usuarios para la publicacion de la actividad
+                Logger.Debug($"[MS-USUARIOS] Preparando publicación de actividad para usuario: {idUsuario}.");
+                var requestUsuario = new RestRequest(Environment.GetEnvironmentVariable("USUARIOS_MS_URL") +
+                                                     $"/publishActivity", Method.Post);
+                var activityBody = new
+                    { IdUsuario = idUsuario, accion = $"Log in." };
+                requestUsuario.AddJsonBody(activityBody);
+
+                Logger.Debug($"[MS-USUARIOS] Enviando POST a {requestUsuario}. Payload: {{ userId: {idUsuario}, activityType: 'UserLogIn' }}");
+                var responseUsuario = await _restClient.ExecuteAsync(requestUsuario);
+                if (!responseUsuario.IsSuccessful)
+                {
+                    Logger.Error($"[500] ERROR DE COMUNICACIÓN EXTERNA [MS-USUARIOS]: El registro del Log in del usuario {idUsuario} fue exitoso, " +
+                                 $"pero falló la publicación de la actividad del usuario {idUsuario}." +
+                                 $" StatusCode: {responseUsuario.StatusCode}, Content: {responseUsuario.Content}",
+                        responseUsuario.ErrorException);
+                    return StatusCode(500, "Error al completar la publicación de la actividad del usuario");
+                }
+                Logger.Info($"[MS-USUARIOS] Actividad de usuario {idUsuario} publicada exitosamente. " +
+                            $"Status: {responseUsuario.StatusCode}.");
+
+                await AuditoriaRepository.InsertarAuditoriaGateway(idUsuario, "INFO", "USUARIO_LOGIN", 
+                    $"El usuario con ID '{idUsuario}' acaba de iniciar sesión en el sistema.");
+
+
                 Logger.Info($"Login completado exitosamente para {request.Correo}. Retornando respuesta (200 OK).");
                 return Ok(new
                 {
@@ -71,10 +113,17 @@ namespace Gateway.API.Controllers
         #endregion
 
         #region Register
+        /// <summary>
+        /// Registra un nuevo usuario en Keycloak, le asigna un rol y envía un correo de verificación.
+        /// </summary>
+        /// <param name="request">Datos del nuevo usuario (correo, contraseña, rol).</param>
+        /// <returns>ID del usuario creado (201 Created) o error (400 Bad Request / 500 Internal Server Error).</returns>
         [HttpPost("register")]
+        [ProducesResponseType((int)HttpStatusCode.Created, Type = typeof(object))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> Register([FromBody] createUserDto request)
         {
-            Logger.Info($"Iniciando registro para el usuario: {request.Correo}");
             Logger.Info($"Iniciando registro para el usuario: {request.Correo}");
             string idKeycloak = null;
             string idRol = request.idRol;
@@ -111,6 +160,9 @@ namespace Gateway.API.Controllers
 
                 Logger.Info($"Proceso de registro completado exitosamente para: {request.Correo}. Retornando Ok (201).");
 
+                await AuditoriaRepository.InsertarAuditoriaGateway(idKeycloak, "INFO", "USUARIO_REGISTER",
+                    $"El usuario con ID '{idKeycloak}' acaba de registrarse en el sistema.");
+
                 return CreatedAtAction(nameof(Register), new { id = idKeycloak }, new
                 {
                     idusuario = idKeycloak,
@@ -127,7 +179,15 @@ namespace Gateway.API.Controllers
         #endregion
 
         #region Reset-Password (Olvidar contraseña)
+        /// <summary>
+        /// Inicia el proceso de restablecimiento de contraseña enviando un correo con un enlace a Keycloak.
+        /// </summary>
+        /// <param name="email">El correo electrónico del usuario.</param>
+        /// <returns>200 OK si el correo fue enviado, o error 400/500.</returns>
         [HttpPost("reset-password/{email}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> ResetPassword([FromRoute] string email)
         {
             Logger.Info($"Solicitud de restablecimiento de contraseña para: {email}");
@@ -162,6 +222,9 @@ namespace Gateway.API.Controllers
                         return BadRequest($"Error al enviar correo: {emailResponse.ErrorMessage}");
                     }
 
+                    await AuditoriaRepository.InsertarAuditoriaGateway(id, "INFO", "USUARIO_RESET_PSSWRD",
+                        $"El usuario con ID '{id}' acaba de cambiar su contraseña ya que olvidó la original.");
+
                     Logger.Info($"Correo de restablecimiento enviado con éxito a {email}.");
                     return Ok("Correo Enviado!");
                 }
@@ -180,8 +243,17 @@ namespace Gateway.API.Controllers
         #endregion
 
         #region Update-Password (Cambiar contraseña)
+        /// <summary>
+        /// Permite a un usuario autenticado cambiar su propia contraseña.
+        /// </summary>
+        /// <param name="request">Contraseña nueva.</param>
+        /// <returns>200 OK si la contraseña fue actualizada, o error 401/400/500.</returns>
         [HttpPost("update-password")]
         [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         public async Task<IActionResult> UpdatePassword([FromBody] actualizarContrasenaDto request)
         {
             try
@@ -217,6 +289,31 @@ namespace Gateway.API.Controllers
                 Logger.Info("Contraseña actualizada exitosamente en Keycloak.");
 
                 await this._sendEmailService.SendCorreoActualizacionContrasenaConfirmacion(email);
+
+                //Conexion con el MS de Usuarios para la publicacion de la actividad
+                Logger.Debug($"[MS-USUARIOS] Preparando publicación de actividad para usuario: {id}.");
+                var requestUsuario = new RestRequest(Environment.GetEnvironmentVariable("USUARIOS_MS_URL") +
+                                                     $"/publishActivity", Method.Post);
+                var activityBody = new
+                    { idUsuario = id, accion = $"Actualización de Contraseña." };
+                requestUsuario.AddJsonBody(activityBody);
+
+                Logger.Debug($"[MS-USUARIOS] Enviando POST a {requestUsuario}. Payload: {{ userId: {id}, activityType: 'UserLogIn' }}");
+                var responseUsuario = await _restClient.ExecuteAsync(requestUsuario);
+                if (!responseUsuario.IsSuccessful)
+                {
+                    Logger.Error($"[500] ERROR DE COMUNICACIÓN EXTERNA [MS-USUARIOS]: El registro del Cambio de contraseña del usuario {id} fue exitoso, " +
+                                 $"pero falló la publicación de la actividad del usuario {id}." +
+                                 $" StatusCode: {responseUsuario.StatusCode}, Content: {responseUsuario.Content}",
+                        responseUsuario.ErrorException);
+                    return StatusCode(500, "Error al completar la publicación de la actividad del usuario");
+                }
+                Logger.Info($"[MS-USUARIOS] Actividad de usuario {id} publicada exitosamente. " +
+                            $"Status: {responseUsuario.StatusCode}.");
+
+
+                await AuditoriaRepository.InsertarAuditoriaGateway(id, "INFO", "USUARIO_CAMBIAR_PSSWRD",
+                    $"El usuario con ID '{id}' acaba de cambiar su contraseña.");
 
                 Logger.Info($"Proceso de cambio de contraseña completado exitosamente para {email}.");
                 return Ok("Contraseña actualizada correctamente.");
---
+++ Gateway.API/Gateway.Infrastructure/Persistences/Repositories/MongoDB/AuditoriaRepository.cs
@@ -0,0 +1,62 @@
+﻿using Gateway.Infrastructure.Interfaces;
+using log4net;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.Json;
+using Gateway.Infrastructure.Configurations;
+
+namespace Gateway.Infrastructure.Persistences.Repositories.MongoDB
+{
+    /// <summary>
+    /// Repositorio encargado de la persistencia de los registros de auditoría del API Gateway en la base de datos MongoDB.
+    /// Implementa <see cref="IAuditoriaRepository"/>.
+    /// </summary>
+    public class AuditoriaRepository : IAuditoriaRepository
+    {
+        private readonly IMongoCollection<BsonDocument> AuditoriaColexion;
+        private readonly ILog Log;
+        public AuditoriaRepository(AuditoriaDbConfig mongoConfig, ILog log)
+        {
+            AuditoriaColexion = mongoConfig.db.GetCollection<BsonDocument>("auditoriaGateway");
+            Log = log;
+        }
+
+        #region InsertarAuditoriaGateway(string idUsuario, string level, string tipo, string mensaje)
+        /// <summary>
+        /// Inserta un nuevo documento de auditoría en la colección 'auditoriaPagos'.
+        /// </summary>
+        /// <param name="idUsuario">ID del usuario relacionado con el evento (puede ser nulo/vacío si el evento no está autenticado).</param>
+        /// <param name="level">Nivel de criticidad del evento (p. ej., INFO, ERROR, WARN).</param>
+        /// <param name="tipo">Tipo de evento o acción que se está auditando (p. ej., "LoginSuccess", "ApiCallFailed").</param>
+        /// <param name="mensaje">Mensaje detallado o cuerpo del registro de auditoría.</param>
+        /// <returns>Tarea asíncrona completada (no retorna valor).</returns>
+        /// <exception cref="ArgumentException">Lanzada si ocurre un error al intentar la inserción en MongoDB.</exception>
+        public async Task InsertarAuditoriaGateway(string idUsuario, string level, string tipo, string mensaje)
+        {
+            var docId = Guid.NewGuid().ToString();
+            Log.Debug($"[GATEWAY AUDIT] Iniciando inserción de auditoría de gateway. Usuario: {idUsuario}, Level: {level}, Tipo: {tipo}. ID Doc: {docId}");
+
+            try
+            {
+                var documento = new BsonDocument
+                {
+                    { "_id",  docId},
+                    { "idUsuario", idUsuario},
+                    { "level", level},
+                    { "tipo", tipo},
+                    { "mensaje", mensaje},
+                    { "timestamp", DateTime.Now}
+                };
+                await AuditoriaColexion.InsertOneAsync(documento);
+                Log.Info($"[GATEWAY AUDIT] Documento de auditoría de gateway insertado exitosamente. ID de Documento: {docId}");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[GATEWAY AUDIT] Error crítico al intentar insertar la auditoría de gateway. Usuario: {idUsuario}, " +
+                          $"Tipo: {tipo}, Mensaje: {mensaje}.", ex);
+                throw new ArgumentException("Error al insertar la auditoria", ex);
+            }
+        }
+        #endregion
+    }
+}
